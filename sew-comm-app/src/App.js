0eb314072b7a1c397096dea490b41cd8e44c4e35
Kalanika <kalanikaw252@gmail.com>
2019-10-19 13:42:41 +1100

    Add router

--- sew-comm-app/src/App.js
+++ sew-comm-app/src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Link, Route} from 'react-router-dom';
+// import Calendar from './calendar'
+
 import './App.css';
+
+
+class Calendar extends React.Component {
+  render() {
+    return (
+      <h1>Calendar</h1>
+    )
+
+  }
+}
 
 function App() {
   return (
@@ -8,6 +21,26 @@ function App() {
       <section class="welcome-header">
         Hey Gladness
       </section>
+
+      <BrowserRouter>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <section id="navigation-panel">
+                        <ul className="menu-bar" id="menu-bar-list">
+                            <li><Link to={'/calendar'} className="nav-link">Calendar</Link></li>
+                            <li><Link to={'/orders'} className="nav-link">My Orders</Link></li>
+                        </ul>
+                    </section>
+                    
+                </nav>
+
+                <hr/>
+
+
+                <Switch>
+                    <Route exact path='/calendar' component={Calendar} />
+                </Switch>
+
+            </BrowserRouter>
       
     </div>
   );
