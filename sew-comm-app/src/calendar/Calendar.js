cf2a66148faab71897fb16b02ed567c5537e4c79
Kalanika <kalanikaw252@gmail.com>
2019-10-27 22:26:12 +1100

    Change import

--- sew-comm-app/src/calendar/Calendar.js
+++ sew-comm-app/src/calendar/Calendar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Switch, Link, Route} from 'react-router-dom';
-import './Calendar.css';
+import './calendar.css';
 function DayCheckbox(props) {
     return (
         <form>
